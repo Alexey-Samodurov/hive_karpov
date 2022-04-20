94b3fd9da9cc3ab59d3505d0a0438e149fb65bb0
Alexey-Samodurov <70944468+Alexey-Samodurov@users.noreply.github.com>
2022-04-20 14:26:44 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 hive -f './sql/create_database.sql'
 hive -f './sql/create_dicts.sql'
+hive -f './sql/insert_into_dicts.sql'
 hive -f './sql/create_main_table.sql'
 hive -f './sql/create_view.sql'
 hive -f './sql/create_mart.sql'
