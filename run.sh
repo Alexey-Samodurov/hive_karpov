bb7a15ce6e194ff2f4e367e0c6fb44edfb42b729
Alexey-Samodurov <70944468+Alexey-Samodurov@users.noreply.github.com>
2022-04-20 14:35:39 +0300

    change run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-hive -f './sql/create_database.sql'
-hive -f './sql/create_dicts.sql'
-hive -f './sql/insert_into_dicts.sql'
-hive -f './sql/create_main_table.sql'
-hive -f './sql/create_view.sql'
-hive -f './sql/create_mart.sql'
+for file in ./sql/*
+do
+  echo $file
+  hive -f $file
+done
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-for file in ./sql/*
-do
-  echo $file
-done
